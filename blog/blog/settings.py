5c18bc30f17798f7110ebb5b17f7695fff233705
scratkong <scratkong@163.com>
2019-01-11 19:03:37 +0800

    add superuser

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'm_blog',
+    'm_blog.apps.MBlogConfig',
 ]
 
 MIDDLEWARE = [
