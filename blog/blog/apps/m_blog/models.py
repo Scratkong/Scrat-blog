206843a57b1ca1905faa71631e032f382b9e9778
scratkong <scratkong@163.com>
2019-01-11 21:05:06 +0800

    add log.html

--- blog/blog/apps/m_blog/models.py
+++ blog/blog/apps/m_blog/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 # Create your models here.
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+# define my own manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -22,3 +30,18 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args = [self.publish.year,
+                               self.publish.strftime('%m'),
+                               self.publish.strftime('%d'),
+                               self.slug])
+
+
+
+
+
+
